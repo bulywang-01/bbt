e8c9048068a9664613fd6433fe08d870096d7018
bulywang-01 <bulywang@gmail.com>
2026-03-25 18:09:36 +0800

    Add files via upload

--- js/games.js
+++ js/games.js
@@ -7,18 +7,25 @@
     return;
   }
 
-function formatDate(iso) {
+function formatDateToTW(iso) {
   const d = new Date(iso);
+
+  // 轉成台灣時間（UTC+8）
+  d.setHours(d.getHours() + 8);
+
   const y = d.getFullYear();
   const m = d.getMonth() + 1;
   const day = d.getDate();
-  const week = ['日','一','二','三','四','五','六'][d.getDay()];
+  const week = ['日', '一', '二', '三', '四', '五', '六'][d.getDay()];
+
   return `${y}/${m}/${day}（${week}）`;
 }
 
 (async () => {
   const gamesDiv = document.getElementById('games');
   const games = await apiGet('games');
+
+  gamesDiv.innerHTML = '';
 
   games.forEach(g => {
     const div = document.createElement('div');
@@ -27,7 +34,7 @@ function formatDate(iso) {
     div.style.marginBottom = '10px';
 
     div.innerHTML = `
-      <b>${formatDate(g.date)}</b> | ${g.field}<br>
+      <b>${formatDateToTW(g.date)}</b> | ${g.field}<br>
       ${g.category || ''}<br>
       <a href="game.html?game_id=${g.game_id}">→ 出勤點名</a>
     `;
@@ -35,4 +42,5 @@ function formatDate(iso) {
     gamesDiv.appendChild(div);
   });
 })();
+
 
