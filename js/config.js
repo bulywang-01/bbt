46fffba02f9b2a4e331444ce42a518f808ddca47
bulywang-01 <bulywang@gmail.com>
2026-03-25 17:35:40 +0800

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-const API_URL='https://script.google.com/macros/s/XXXXXXXX/exec';
+const API_URL='https://script.google.com/macros/s/AKfycbwCYCuE1FLyvRygzBesUf-Y7PU5JpXTS3l8SkpOP3qZjnCA_ay8ela-RXo8mSZ9Jwd6Jg/exec';
 const ROLE='admin'; const MY_JUDGE_ID='U001';
