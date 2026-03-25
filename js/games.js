11c2866796cc87d769e0499c33808df8dd41c879
bulywang-01 <bulywang@gmail.com>
2026-03-25 18:05:47 +0800

    Add files via upload

--- js/games.js
+++ js/games.js
@@ -7,6 +7,19 @@
     return;
   }
 
+function formatDate(iso) {
+  const d = new Date(iso);
+  const y = d.getFullYear();
+  const m = d.getMonth() + 1;
+  const day = d.getDate();
+  const week = ['日','一','二','三','四','五','六'][d.getDay()];
+  return `${y}/${m}/${day}（${week}）`;
+}
+
+(async () => {
+  const gamesDiv = document.getElementById('games');
+  const games = await apiGet('games');
+
   games.forEach(g => {
     const div = document.createElement('div');
     div.style.border = '1px solid #ccc';
@@ -14,11 +27,12 @@
     div.style.marginBottom = '10px';
 
     div.innerHTML = `
-      <b>${g.date}</b>｜${g.field}<br>
+      <b>${formatDate(g.date)}</b> | ${g.field}<br>
       ${g.category || ''}<br>
-      <a href="game.html?game_id=${g.game_id}">➡ 出勤點名</a>
+      <a href="game.html?game_id=${g.game_id}">→ 出勤點名</a>
     `;
 
     gamesDiv.appendChild(div);
   });
 })();
+
