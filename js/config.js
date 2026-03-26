61e565ca719d3aa83f007299f625b9d18bc0fe3c
bulywang-01 <bulywang@gmail.com>
2026-03-26 14:42:34 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const API_URL='https://script.google.com/macros/s/AKfycbwCYCuE1FLyvRygzBesUf-Y7PU5JpXTS3l8SkpOP3qZjnCA_ay8ela-RXo8mSZ9Jwd6Jg/exec';
+const API_URL='https://script.google.com/macros/s/AKfycbwt9xR7A6AW996FHZpP_UCNH7iwn7WaWjmv_G2tU7r7L_TvfewqkfEdTBh_2C79949k/exec';
 // const ROLE='admin'; const MY_JUDGE_ID='U001';
 // const API_URL = '你的 Apps Script Web App URL';
 
