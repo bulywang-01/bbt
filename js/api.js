22276d28b10efe397b39d9415e3a893224e58cb0
bulywang-01 <bulywang@gmail.com>
2026-03-26 14:20:28 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbwCYCuE1FLyvRygzBesUf-Y7PU5JpXTS3l8SkpOP3qZjnCA_ay8ela-RXo8mSZ9Jwd6Jg/exec';
+// const API_URL = 'https://script.google.com/macros/s/AKfycbwCYCuE1FLyvRygzBesUf-Y7PU5JpXTS3l8SkpOP3qZjnCA_ay8ela-RXo8mSZ9Jwd6Jg/exec';
 
 async function apiPost(data) {
   const res = await fetch(API_URL, {
