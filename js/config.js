399d3a1f870d1236ee554f6b81891d1e89f80aa6
bulywang-01 <bulywang@gmail.com>
2026-03-31 16:28:50 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const API_URL='https://script.google.com/macros/s/AKfycbxkm1YpsgOHGKdyU53a-SFKP0mOYf3YcQ7PE6NaccDxtiCJ2jrvFOXbQp3sH9STlZSH3g/exec';
+const API_URL='https://script.google.com/macros/s/AKfycbyWDD7yygbgC4KnJ6ZeX6DTkThkNEp1IbRoCuX98RSv0EEmWDjkUGIJt8yuoCCW4hEJ4w/exec';
