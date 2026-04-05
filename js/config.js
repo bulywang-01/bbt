47b865a75fc2d3c20708ca4819704005f987c5cc
bulywang-01 <bulywang@gmail.com>
2026-04-05 11:10:22 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,20 +1,20 @@
 // ===== Apps Script Web App URL（務必確認正確）=====
-const API_URL='https://script.google.com/macros/s/AKfycbxjo1jDShECSvgUkoPTjYslmLXkSHI_sEvoui0L9TVQL7xofLTfk3iDA59GP6cdhWfu/exec';
+const API_URL='https://script.google.com/macros/s/AKfycbzqocrhzWj7MlJvuHvhex5OCW_FLQ0pgcEyTS54y8oo89Ml7qbfeZrogomUReX6WMFC/exec';
 
 // ===== JSONP 呼叫工具 =====
-function callApi(query, callback) {
-  const cbName = 'cb_' + Date.now();
+//function callApi(query, callback) {
+//  const cbName = 'cb_' + Date.now();
 
-  window[cbName] = function (res) {
-    try {
-      callback(res);
-    } finally {
-      delete window[cbName];
-      script.remove();
-    }
-  };
+//  window[cbName] = function (res) {
+//    try {
+//      callback(res);
+//    } finally {
+//      delete window[cbName];
+//      script.remove();
+//    }
+//  };
 
-  const script = document.createElement('script');
-  script.src = API_URL + '?' + query + '&callback=' + cbName;
-  document.body.appendChild(script);
-}
+//  const script = document.createElement('script');
+//  script.src = API_URL + '?' + query + '&callback=' + cbName;
+//  document.body.appendChild(script);
+//}
